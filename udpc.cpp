b7fd393fbd93ad8bb806b7f6e99d359885d49cda
plantver <plantver@gmail.com>
2016-03-08 15:18:02 -0800

    new structure

--- udpc.cpp
+++ udpc.cpp
@@ -8,77 +8,19 @@
 #include <netdb.h>      // define structures like hostent
 #include <string.h>	/* for memcpy */
 
+// the protocal
+#include "connection.hpp"
+
 
 int server(int portno){
-	int socketfd;
-	if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("cannot create socket");
-		return 0;
-	}
-
-	//my socket
-	struct sockaddr_in myaddr;
-	memset(&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(portno); //0 means any, in case of client
-
-	if (bind(socketfd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("server bind socket failed");
-		return 0;
-	}
-
-	//client socket
-	struct sockaddr_in cliaddr;
-	memset(&myaddr, 0, sizeof(cliaddr));
-	socklen_t cliaddrlen;
-
-	const int BUFSIZE = 1024;
-	unsigned char buf[BUFSIZE];
-	int recvlen = 0;
-	while(1){
-		printf("Waiting on port %d\n", portno);
-		recvlen = recvfrom( socketfd, buf, BUFSIZE, 0, (struct sockaddr *)&cliaddr, &cliaddrlen );
-        printf("received %d bytes\n", recvlen);
-	    if (recvlen > 0) {
-	            buf[recvlen] = 0;
-	            printf("received message: \"%s\"\n", buf);
-	    }
-	}
+	Connection con(portno);
+	(void*)con.waitforreq();
 }
 
-
-int client( struct hostent* server, int servportno ){
-
-	//server socket
-	struct sockaddr_in servaddr;
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	memcpy((void *)&servaddr.sin_addr, server->h_addr_list[0], server->h_length);
-	servaddr.sin_port = htons(servportno);
-
-	//my socket
-	struct sockaddr_in myaddr;
-	memset(&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(0); //0 means any, in case of client
-	int socketfd;
-	if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("cannot create socket");
-		return 0;
-	}
-	if (bind(socketfd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("client bind socket failed");
-		return 0;
-	}
-
-	char* message = "test message";
-	if (sendto(socketfd, message, strlen(message), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-		perror("client sendto failed");
-		return 0;
-	}
-
+int client( char* server, int servportno ){
+	Connection con(server, servportno);
+	char* mes = "test message";
+	con.senddg('R', 0, mes, strlen(mes));
 }
 
 
@@ -90,13 +32,8 @@ int main(int argc, char *argv[]){
         server(portno);
     }
     else if (argc == 3) { //open up client
-    	struct hostent *server; //contains tons of information, including the server's IP address
-    	if(!(server = gethostbyname(argv[1]))){ //takes a string like "www.yahoo.com", and returns a struct hostent which contains information, as IP address, address type, the length of the addresses...
-    		printf("error getting hostend \n");
-    		exit(0);
-    	}
     	portno = atoi(argv[2]);
-    	client( server, portno );
+    	client( argv[1], portno );
     }
     else{
     	printf("enter port for server, or server address and port for client \n");
